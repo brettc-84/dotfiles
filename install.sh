e4862631e1dffbf76aa4cbf347d094e06462fedd
brettc-84 <brett.coomer@gmail.com>
2018-10-29 11:24:02 +0200

    bash_profile

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ PATH="$DOTFILES_DIR/bin:$PATH"
 
 # Bunch of symlinks
 
-# ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
+ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
 # ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
 # ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 # ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
