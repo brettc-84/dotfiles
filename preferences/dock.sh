8a10455f198d56e4750c74bdffc73021219e9948
brettc-84 <brett.coomer@gmail.com>
2018-11-19 21:56:11 +0200

    some dock preferences

--- preferences/dock.sh
+++ preferences/dock.sh
@@ -5,12 +5,21 @@ fi
 
 # Tile size
 defaults write com.apple.dock tilesize -integer 32
+# Enable magnification
+defaults write com.apple.dock magnification -bool TRUE
 # Magnification size
 defaults write com.apple.dock largesize -int 64
 # Animation speed
 defaults write com.apple.dock autohide-time-modifier -float 0.2
 # Window effect
 defaults write com.apple.dock mineffect -string suck
+# Show hidden apps as transparent tiles
+defaults write com.apple.dock showhidden -bool TRUE
+# Minimise window to app icon
+defaults write com.apple.dock minimize-to-application -int 1
 
 # Restart dock
 killall Dock
+
+# resets to default settings
+# defaults delete com.apple.dock; killall Dock
