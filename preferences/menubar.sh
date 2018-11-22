245cabce61366c0919c24864e881901de5dbb904
Brett Coomer <brett.coomer@gmail.com>
2018-11-22 18:06:23 +0200

    remove user from menu bar

--- preferences/menubar.sh
+++ preferences/menubar.sh
@@ -1,13 +1,19 @@
-# if ! is-macos -o; then
-#   echo "Skipped: Dock (Not MacOS)"
-#   return
-# fi
+if ! is-macos -o; then
+  echo "Skipped: Menubar (Not MacOS)"
+  return
+fi
+
+# Remove menu items
+defaults write com.apple.systemuiserver dontAutoLoad -array \
+ "/System/Library/CoreServices/Menu Extras/User.menu"
 
 # Add menu items
 defaults write com.apple.systemuiserver menuExtras -array \
+#  "/System/Library/CoreServices/Menu Extras/Displays.menu" \
  "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
- "/System/Library/CoreServices/Menu Extras/Clock.menu"
-#  "/System/Library/CoreServices/Menu Extras/Displays.menu"
+ "/System/Library/CoreServices/Menu Extras/Clock.menu" \
+ "/System/Library/CoreServices/Menu Extras/Battery.menu" \
+ "/System/Library/CoreServices/Menu Extras/AirPort.menu"
 
 # Clock
 # Should not be analog
