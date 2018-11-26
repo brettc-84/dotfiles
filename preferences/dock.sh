d6cbaaf3d36663e9b1a93750f7f707ece4e7f63b
Brett Coomer <brett.coomer@gmail.com>
2018-11-26 10:34:00 +0200

    dock hide time modifier

--- preferences/dock.sh
+++ preferences/dock.sh
@@ -10,7 +10,7 @@ defaults write com.apple.dock magnification -bool TRUE
 # Magnification size
 defaults write com.apple.dock largesize -int 64
 # Animation speed
-defaults write com.apple.dock autohide-time-modifier -float 0.2
+defaults write com.apple.dock autohide-time-modifier -float 0
 # Window effect
 defaults write com.apple.dock mineffect -string suck
 # Show hidden apps as transparent tiles
